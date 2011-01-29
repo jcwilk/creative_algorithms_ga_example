28b5a47e3ccc9ee0186132bf86d891de54d2bd90
jcwilk <jcwilk@gmail.com>
2011-01-28 18:04:32 -0800

    Added some whitespace

--- algorithm.rb
+++ algorithm.rb
@@ -16,10 +16,12 @@ end
 
 def point_mutation(bitstring, rate=1.0/bitstring.size)
   child = ""
+
   bitstring.size.times do |i|
     bit = bitstring[i].chr
     child << ((rand()<rate) ? ((bit=='1') ? "0" : "1") : bit)
   end
+
   return child
 end
 
@@ -31,6 +33,7 @@ end
 
 def reproduce(selected, pop_size, p_cross, p_mutation)
   children = []
+
   selected.each_with_index do |p1, i|
     p2 = (i.modulo(2)==0) ? selected[i+1] : selected[i-1]
     p2 = selected[0] if i == selected.size-1
@@ -40,6 +43,7 @@ def reproduce(selected, pop_size, p_cross, p_mutation)
     children << child
     break if children.size >= pop_size
   end
+
   return children
 end
 
@@ -47,8 +51,10 @@ def search(max_gens, num_bits, pop_size, p_crossover, p_mutation)
   population = Array.new(pop_size) do |i|
     {:bitstring=>random_bitstring(num_bits)}
   end
+
   population.each{|c| c[:fitness] = onemax(c[:bitstring])}
   best = population.sort{|x,y| y[:fitness] <=> x[:fitness]}.first
+
   max_gens.times do |gen|
     selected = Array.new(pop_size){|i| binary_tournament(population)}
     children = reproduce(selected, pop_size, p_crossover, p_mutation)
@@ -59,6 +65,7 @@ def search(max_gens, num_bits, pop_size, p_crossover, p_mutation)
     puts " > gen #{gen}, best: #{best[:fitness]}, #{best[:bitstring]}"
     break if best[:fitness] == num_bits
   end
+
   return best
 end
 
@@ -72,5 +79,6 @@ if __FILE__ == $0
   p_mutation = 1.0/num_bits
   # execute the algorithm
   best = search(max_gens, num_bits, pop_size, p_crossover, p_mutation)
+
   puts "done! Solution: f=#{best[:fitness]}, s=#{best[:bitstring]}"
 end
